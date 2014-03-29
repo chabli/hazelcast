fad953b1a3cd706f172557c93a32aba457b52371
Yassine CHABLI <yassine.chabli@gmail.com>
2014-03-29 19:22:12 +0100

    init sync.pl

---
+++ script/sync.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use LWP::Simple;
+use JSON qw( decode_json );
+use Try::Tiny;
+
+
+print 'Host IP (example: 10.142.66.115): ';
+my $host = <>;
+chomp $host;
+
+print 'Port Number (example: 9010): ';
+my $port = <>;
+chomp $port;
+
+print 'Map Name (example: AFS_VSI_Authenticate_v1): ';
+my $map = <>;
+chomp $map;
+
+#TODO : cluster var to setup
+
+try{
+	my $contents = get("http://$host:$port/mancenter-3.1.5/main.do?cluster=PPRD&operation=memberlist_instancelist_alertPopup_versionMismatch_charts_dataTablesMap&type=map&instance=$map&chart1type=i_Map_OwnedEntryCount&chart2type=o_Map_total&throughputInterval=60000&curtime=0");
+	my $decodedJson = decode_json($contents);
+	my $noeud1IP = $decodedJson->{'fillMapMemoryTable'}['0']['1'];
+	my $noeud2IP = $decodedJson->{'fillMapMemoryTable'}['1']['1'];
+	my $noeud1Entries = $decodedJson->{'fillMapMemoryTable'}['0']['2'];
+	my $noeud2Entries = $decodedJson->{'fillMapMemoryTable'}['1']['2'];
+	my $noeud1Backups = $decodedJson->{'fillMapMemoryTable'}['0']['4'];
+	my $noeud2Backups = $decodedJson->{'fillMapMemoryTable'}['1']['4'];
+	print "\nNode 1  = $noeud1IP  -  Entries: $noeud1Entries - Backups_Entries: $noeud1Backups  \n";
+	print "Node 2  = $noeud2IP  -  Entries: $noeud2Entries - Backups_Entries: $noeud2Backups  \n\n";
+
+	my $loopContinue = 1;
+	my $sleepSec = 2;
+	my $waitingTime = 0;
+	while ($loopContinue) {
+		my $contents = get("http://$host:$port/mancenter-3.1.5/main.do?cluster=PPRD&operation=memberlist_instancelist_alertPopup_versionMismatch_charts_dataTablesMap&type=map&instance=$map&chart1type=i_Map_OwnedEntryCount&chart2type=o_Map_total&throughputInterval=60000&curtime=0");	
+		my $decodedJson = decode_json($contents);
+		my $noeud1Entries = $decodedJson->{'fillMapMemoryTable'}['0']['2'];
+		my $noeud2Entries = $decodedJson->{'fillMapMemoryTable'}['1']['2'];
+		my $noeud1Backups = $decodedJson->{'fillMapMemoryTable'}['0']['4'];
+		my $noeud2Backups = $decodedJson->{'fillMapMemoryTable'}['1']['4'];
+		
+		if ($noeud1Backups == $noeud2Entries &&  $noeud2Backups == $noeud1Entries) {
+			print "\nNodes are synchronized \n";
+			$loopContinue = 0;
+
+		}else{
+			print "\nWarning ! Synchronization in progress... \n";
+			sleep($sleepSec);
+		}
+
+		$waitingTime += $sleepSec;
+		if($waitingTime % 10 == 0)  {
+			print 'Do you want to continue ? (y/n) ';
+			my $choice = <>;
+			chomp $choice;
+			if (lc($choice) eq 'n') {
+		 		last;
+		 	}
+		 } 	
+	}
+}catch{
+	warn "caught error: $_";
+};
